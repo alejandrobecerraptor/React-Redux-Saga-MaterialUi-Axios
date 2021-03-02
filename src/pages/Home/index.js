110a41b1764ad99e0bf7d63e4938816a19d5ec6f
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-02 10:44:26 -0500

    creando el componente del home, el html

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, {useState} from 'react';
+import { Container, Typography, Card, Grid , TextField, Button } from '@material-ui/core';
 
-export default () => (
-	<Container>
-		<Typography variant="h4">
-			Home
-		</Typography> 
-	</Container>
-)
+export default () => {
+
+	const [searchText , setSearchText] = useState ('');
+
+	const handleSearchTextChange = event =>{
+		setSearchText(event.target.value);
+	};
+
+	const handleCleanTextClick = event =>{
+		console.log(10);
+	};
+
+	const handleSearchTextClick = event =>{
+		console.log(20);
+	};
+	return (
+		<Container>
+			<Card>
+				<Grid container>
+					<Grid>
+						<Typography>
+							Bievenido !
+						</Typography>
+					</Grid>
+						<label>Icono</label>
+					<Grid>
+						
+					</Grid>
+				</Grid>
+				<TextField
+					value={searchText}
+					placeholder="Buscar..."
+					onChange={handleSearchTextChange}
+				/>
+				<Grid>
+					<Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
+					<Button variant="contained" color="primary" size="large" onClick={handleSearchTextClick} >Buscar</Button>
+				</Grid>
+			</Card>
+		</Container>
+	)
+}

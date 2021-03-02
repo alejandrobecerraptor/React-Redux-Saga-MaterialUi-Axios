bc6d3aa08e7bd228d65069d45aca24cb77732d90
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-02 15:59:05 -0500

    cargando resultados que llegan del api

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,15 +1,17 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from '@material-ui/core';
+import { Container,CircularProgress } from '@material-ui/core';
 import queryString from 'query-string';
 
 import { searchMovie } from '../../redux/actions/search';
 import { movieResults, isSearchLoading } from '../../redux/selectors';
+import MovieResult from '../../components/MovieResult';
 
 export default ({location}) => {
 
     const dispatch = useDispatch();
     const movies = useSelector(state => movieResults(state));
+    const isLoading = useSelector(state => isSearchLoading(state));
      console.log(movies);
 
     useEffect(() => {
@@ -20,10 +22,20 @@ export default ({location}) => {
         }
     });
 
+    const renderMovies = () => {
+        if (movies) {
+            return movies.map((value,index) => <MovieResult key={index} {...value}/>);
+        }else if (isLoading){
+            return <CircularProgress size={100} color="primary"/>
+        }
+
+        return <div/>;
+    };
+
 
     return(
         <Container>
-            results
+            {renderMovies()}
         </Container>
     )
 }

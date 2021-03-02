b1828c313e0c9d2837bac8811ec96fb38b163f88
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-02 16:05:27 -0500

    agregando un poco de estilos al componente que me muestra los resultados

--- src/components/MovieResult/index.js
+++ src/components/MovieResult/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Container, Typography, Card, Grid , TextField, Button } from '@material-ui/core';
 
+import style from './style';
+
 const MovieResult = ({Title,Year,Type,imdbId,Poster}) =>{
+    const classes = style();
     return(
-        <Card>
+        <Card className={classes.cardContainer}>
             <Grid container>
                 <Grid item>
-                    <img src={Poster} alt={Title}/>
+                    <img src={Poster} alt={Title} className={classes.poster}/>
                 </Grid>
-                <Grid item>
+                <Grid item className={classes.titlesContainer}>
                     <Typography>{Title}</Typography>
                     <Typography>{Year}</Typography>
                     <Typography>{Type}</Typography>
-                    <Button color="primary" variant="contained">Ber mas</Button>
+                    <Button color="primary" variant="contained">Ver mas</Button>
                 </Grid>
             </Grid>
         </Card>
---
+++ src/components/MovieResult/style.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core';
+import { makeStyle } from '@material-ui/styles';
+
+export default makeStyles({
+    cardContainer: {
+        marginBottom: 8
+    },
+    poster: {
+        width: 220
+    },
+    titlesContainer: {
+        paddingLeft: 8
+    }
+});

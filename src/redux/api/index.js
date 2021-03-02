4962114298d217ee7bee8cc4f0be95e5560ffa01
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-02 11:30:02 -0500

    creando el config de axios, con la api generada de la pagina omdbapi

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
 
+const baseURL = 'https://www.omdbapi.com/?i=tt3896198&apikey=5d27e142';
+
+export const apiCall = (url,data,headers,method) => axios({
+    method,
+    url: baseURL + url,
+    data,
+    headers
+});

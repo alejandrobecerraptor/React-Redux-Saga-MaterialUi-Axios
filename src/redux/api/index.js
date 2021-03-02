2759acf4f5b02b123b1b1978ded6a4177bef5d27
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-02 10:25:03 -0500

    se inicializa el proyecto, creacion , instalacion de material UI y distrubucion de carpetas

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,9 +1,1 @@
-const axios = require('axios');
 
-export const getDemo = params => {
-	const URL = 'https://reactjsteachingproj.herokuapp.com/users';
-
-	return axios.get(URL).then(response => {
-		return response.data;
-	});
-};
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,6 +1,5 @@
 import { all } from 'redux-saga/effects';
-import watchDemo from './watcher';
 
 export default function* rootSaga() {
-	yield all([watchDemo()]);
+	yield all([]);
 }
